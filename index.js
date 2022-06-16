54b3660d627d71d21bef378397e93f21a105e833
Jan Strnádek <jan.strnadek@gmail.com>
2022-06-16 08:59:31 +0200

    Fix import and parsing

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const core = require("@actions/core");
 const AWS = require("aws-sdk");
-const wmatch = require("wildcard-match");
 
 const s3 = new AWS.ECS();
 const resourcegroupstaggingapi = new AWS.ResourceGroupsTaggingAPI();
 
+const parseJsonFromMultiline = (value) => {
+  if (Array.isArray(value)) {
+    return JSON.parse(value[0]);
+  } else {
+    return JSON.parse(value);
+  }
+}
+
 const main = async () => {
   const cluster = core.getInput("cluster", { required: true });
-  const tagFilters = JSON.parse(core.getMultilineInput("tag_filters", { required: true }));
+  const tagFilters = parseJsonFromMultiline(core.getMultilineInput("tag_filters", { required: true }));
   
   core.debug("Finding services with proper tags...");
 
